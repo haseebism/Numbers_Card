7c9b362762f33fd7481c995a6aea4236e45c75dc
haseebism <haseebzulfiqar2593@gmail.com>
2021-11-03 00:24:28 -0700

    Numbers card game

---
+++ Numberscard/lib/splash.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:quizstar/home.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 20), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => homepage(),
+      ));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black45,
+      body: Center(
+        child: Text(
+          "Hello Kids\n Are You Ready!",
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: "Satisfy",
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
